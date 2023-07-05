07f35c12ce58efdbeb173d06c00eb2cdd0638193
Retlesh <92025548+Retlesh@users.noreply.github.com>
2023-07-05 19:43:25 +0200

    Clear comments

--- src/IndexCalculations.js
+++ src/IndexCalculations.js
@@ -3,9 +3,6 @@ export function isLeapYear(year) {
 }
 
 export function countDaysBetweenDates(date1, date2) {
-    //     date1 = date1.replace('-', ',');
-    //     date2 = date2.replace('-', ',');
-    // console.log(date1, date2);
     // Convert the dates to UTC to avoid timezone differences
     const utcDate1 = new Date(date1.toUTCString());
     const utcDate2 = new Date(date2.toUTCString());
